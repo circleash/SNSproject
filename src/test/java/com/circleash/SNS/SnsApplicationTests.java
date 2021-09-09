907232d3e9a07c8b42de582e24e68c2eb2496d99
DESKTOP-4GKAT56\JE <circleash92@gmail.com>
2021-09-09 14:04:37 +0900

    [최원제]  - 최초 파일추가

---
+++ src/test/java/com/circleash/SNS/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circleash.SNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
